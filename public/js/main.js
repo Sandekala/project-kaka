b718d16bf19fd1223b23d3a19af1e6d6ce9e1140
Sandekala <diimasaziiz@google.com>
2022-08-08 20:11:27 +0700

    tambah animasi

--- public/js/main.js
+++ public/js/main.js
@@ -7,10 +7,31 @@ window.addEventListener('scroll', () => {
     header.classList.remove('navbar-fixed');
   }
 });
-
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('navMenu');
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
-  navMenu.classList.toggle('hidden');
+  navMenu.classList.toggle('navHide');
+});
+
+const colapse = document.querySelectorAll('.colapse');
+colapse.forEach((colaps) => {
+  colaps.addEventListener('click', () => {
+    const hide = colaps.children[2];
+    const colapsing = colaps.children[1].children[0];
+    const colapsed = colaps.children[1].children[1];
+    hide.classList.toggle('colapseHide');
+    colapsing.classList.toggle('hidden');
+    colapsed.classList.toggle('hidden');
+  });
+});
+
+const hubBtn = document.getElementById('hubBtn');
+const hub = document.getElementById('hub');
+hubBtn.addEventListener('click', () => {
+  const icons = hub.children;
+  for (let i = 0; i < icons.length; i++) {
+    icons[i].classList.toggle('iconHide');
+  }
 });
